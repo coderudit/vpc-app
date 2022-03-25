a23387c077f40e43d3280d3bc4e697ba885342bf
Udit Gandhi <coder.udit@gmail.com>
2022-03-25 12:57:15 -0300

    removed duplicate connect

--- controllers/students.js
+++ controllers/students.js
@@ -44,18 +44,18 @@ const storeStudentsInRDS = async (req, res) => {
 
 const getStudentsFromRDS = async (req, res) => {
   secretManager();
-  db.connect((err) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    console.log("Querying data.");
+  // db.connect((err) => {
+  //   if (err) {
+  //     console.log(err);
+  //     return;
+  //   }
+  console.log("Querying data.");
 
-    db.query(`SELECT * FROM students;`, function (err, result, fields) {
-      if (err) res.send(err);
-      if (result) res.send(result);
-    });
+  db.query(`SELECT * FROM students;`, function (err, result, fields) {
+    if (err) res.send(err);
+    if (result) res.send(result);
   });
+  // });
 };
 
 module.exports = { storeStudentsInRDS, getStudentsFromRDS };
